8835c0c51f8bb71687c0f112ade04804a7b39bbc
stormfather0 <ivan.ystskovyna@gmail.com>
2025-01-29 16:18:17 +0200

    Rebuild bcrypt or switch to bcryptjs

--- my-backend/server.js
+++ my-backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors'; 
-import bcrypt from 'bcrypt';
+// import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import path from 'path'; 
 import { fileURLToPath } from 'url'; 
