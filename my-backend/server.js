2ac5186e1ceb3d4225931ac1b306cc441fbc47d0
stormfather0 <ivan.ystskovyna@gmail.com>
2025-01-29 17:32:11 +0200

    Fix duplicates

--- my-backend/server.js
+++ my-backend/server.js
@@ -425,11 +425,11 @@ app.get('/api/products/:id', (req, res) => {
   //
 
 // Use CORS for frontend requests
-app.use(cors({
-    origin: 'http://127.0.0.1:5500',
-    methods: ['POST', 'GET', 'OPTIONS', 'DELETE'], 
-    allowedHeaders: ['Content-Type'], 
-  }));
+// app.use(cors({
+//     origin: 'http://127.0.0.1:5500',
+//     methods: ['POST', 'GET', 'OPTIONS', 'DELETE'], 
+//     allowedHeaders: ['Content-Type'], 
+//   }));
   
   app.use(express.json());
   
