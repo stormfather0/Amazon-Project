29261bab17f0e98712ac09b8a69cd27aadb96080
stormfather0 <ivan.ystskovyna@gmail.com>
2025-02-25 10:16:11 +0200

    test

--- my-backend/server.js
+++ my-backend/server.js
@@ -97,7 +97,12 @@ const corsOptions = {
 };
 app.use(cors(corsOptions));  
 
-  
+app.options('*', (req, res) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.sendStatus(200);
+});
 
 app.use(express.json());
 
