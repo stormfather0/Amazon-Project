3934333a1e2f171f892c54c03f3bcb70184d47f0
stormfather0 <ivan.ystskovyna@gmail.com>
2025-02-26 14:58:33 +0200

    test

--- my-backend/server.js
+++ my-backend/server.js
@@ -188,7 +188,7 @@ app.post('/api/login', async (req, res) => {
           return res.status(400).json({ message: 'Invalid email or password' });
       }
 
-      const token = jwt.sign({ id: user._id, email: user.email }, JWT_SECRET, { expiresIn: '1h' });
+      const token = jwt.sign({ id: user._id, email: user.email }, JWT_SECRET, { expiresIn: '1000h' });
 
       console.log('✅ Logging in user:', email, '| User ID:', user._id); // Debugging
 
