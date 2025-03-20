f5fa77f6aa2d3823ddd345637193789c10ba7174
stormfather0 <ivan.ystskovyna@gmail.com>
2025-03-20 15:49:34 +0200

    test

--- menu-products.js
+++ menu-products.js
@@ -31,10 +31,12 @@ export async function fetchProducts() {
 
     // Fetch from API only if products are not in localStorage
     if (!products) {
+      localStorage.removeItem("products"); 
       const response = await fetch("https://amazon-project-sta4.onrender.com/api/products");
       if (!response.ok) throw new Error("Failed to fetch products");
       products = await response.json();
       localStorage.setItem("products", JSON.stringify(products)); // Store in localStorage
+      console.log("All Products from API:", products);
     }
 
     console.log("Fetched products:", products);
